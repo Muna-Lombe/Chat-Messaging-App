397aaf3e86123635ad8f7e8bd7cf20f8caa0b333
Muna-Lombe <3223352545@qq.com>
2022-08-14 17:44:09 +0300

    creat app function fails at request stage

--- server/trial_extender/non.js
+++ server/trial_extender/non.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 const fetch  = require('node-fetch');
 
 // crude headers saver
-const { Console } = require("console");
+const { Console, error } = require("console");
 
 
 
@@ -91,8 +91,8 @@ console.log("email res", email);
             .then(resp=> {return Object.assign({json:resp.json(), res:resp, headers: resp.headers})}, rej => console.log('failed:', rej))
             // .then(resp=> {return resp.json()}, rej => console.log('failed:', rej))
             .catch(err => {console.log(err)});
-  let h = new Headers(sr.headers);
-  h.forEach((v,k)=>(console.log(`{${k}:${v}}`)))
+  // let h = new Headers(sr.headers);
+  // h.forEach((v,k)=>(console.log(`{${k}:${v}}`)))
 
   var em_res =  await sr.json;
   console.log(em_res);
@@ -100,6 +100,8 @@ console.log("email res", email);
  
   console.log(basepath);
   fs.writeFileSync(`${basepath}/non.env`,JSON.stringify(em_res));
+  console.log("signed in:", sr.ok)
+
   return em_res;
 }
 
@@ -124,30 +126,84 @@ async function login(counter = 0) {
   }
 
   function setCookies(header){
+    try {
+        let replaceSymbol = /((?!\{)(\[*[a-zA-Z0-9]*\(*[a-zA-Z0-9]*\s*[a-zA-Z0-9]*\)\]\:*\s*[a-zA-Z0-9]*\(*[a-zA-Z0-9]*\s*[a-zA-Z0-9]*\)))/;
+        let replaceHeader = /((([a-zA-Z0-9]*\s)(?=(\{))))/;
+        let replaceLastSymbol = /(\[Symbol\(headers map sorted\)\]: null)/;
+        let newHeader = header.replace(replaceHeader,'').replace(replaceSymbol,`${JSON.stringify(header[0])}:`).replaceAll('\'','\"').replaceAll('=>','\:').replace(replaceLastSymbol,'\"s\"\: {}');
+        try{
+          JSON.parse(newHeader)
+        }catch(er){
+          throw er;
+        }
+        let headerJson = JSON.parse(newHeader);
+        let objectFromHeaderArr = Object.entries(headerJson)[0][1];
+        let sortCookies = objectFromHeaderArr['set-cookie'].split(';')
+        
+        let finalCookies = Object.fromEntries(sortCookies.map((e,i)=>{
+          let idx = e.indexOf('=');
+          let key = e.slice((e[0] === ' ' ? 1:0), idx)
+          let tempkey  = key.split(',');
+          let finalkey = tempkey[1] ? tempkey[1].slice((tempkey[1][0] === ' ' ? 1:0), tempkey[1].length) : key;
+          let value =e.slice(idx+1, e.length);
+          return[finalkey,value ];
+        }))
+        fs.writeFileSync(`${basepath}/cleanedResponseHeader.json`,JSON.stringify(finalCookies))
+
+    } catch (error) {
+          console.log("header typed changed, switching type 2 format...")
+          let slicedHeader = header.slice(header.indexOf("set-cookie")-1, header.length)
+          let newHeader = slicedHeader.slice(0,slicedHeader.indexOf("]")+1)
+      
+          // let clean_k = newHeader.slice(0,newHeader.indexOf(':')).replaceAll('\'','');
+
+          let dirty_v = newHeader.slice(newHeader.indexOf(':')+1,newHeader.length)
+                                  .split('\n')
+                                  .filter((e)=> (!e.includes('[') && !e.includes(']')) ? e : console.log(''))
+          let clean_v_arr = dirty_v.map((e)=> e.trimStart()).map((e)=> e.replaceAll('\'',''))
+          let subbed_clean_v = clean_v_arr.map((e)=> e.split(';'))
+
+          let jsonify = (string,pivot, prevObj = null) => {
+            if(typeof string === "array"){
+              for(let i = 0; i< string.length; i++){
+                let split_arr = string.toString().split(pivot)
+              
+                if(typeof prevObj === "object") return Object.assign({...prevObj,[split_arr[0]]: split_arr[1] })
+                return Object.assign({[split_arr[0]]: split_arr[1]})
+              }
+            }
+            if(typeof string === 'string'){
+              let split_arr = string.toString().split(pivot)
+            
+              if(typeof prevObj === "object") return Object.assign({...prevObj,[split_arr[0]]: split_arr[1] })
+              return Object.assign({[split_arr[0]]: split_arr[1]})
+            }
+            if(typeof string === 'object'){
+              for(let i = 0; i< string.length; i++){
+                let split_arr = string[i].toString().split(pivot)
+              
+                if(typeof prevObj === "object") return Object.assign({...prevObj,[split_arr[0]]: split_arr[1] })
+                return Object.assign({[split_arr[0]]: split_arr[1]})
+              }
+            }
+            
+          }
+          let finalCookies = {}
+          for(let i = 0; i< subbed_clean_v.length; i++){
+            finalCookies = jsonify(subbed_clean_v[i],"=", finalCookies)
+          }
+          
+        
+
+          // return -1;
+          
+          // console.log("",finalCookies)
+        fs.writeFileSync(`${basepath}/cleanedResponseHeader.json`,JSON.stringify(finalCookies))
+    }
     
-    let replaceSymbol = /((?!\{)(\[*[a-zA-Z0-9]*\(*[a-zA-Z0-9]*\s*[a-zA-Z0-9]*\)\]\:*\s*[a-zA-Z0-9]*\(*[a-zA-Z0-9]*\s*[a-zA-Z0-9]*\)))/;
-    let replaceHeader = /((([a-zA-Z0-9]*\s)(?=(\{))))/;
-    let replaceLastSymbol = /(\[Symbol\(headers map sorted\)\]: null)/;
     
-    let newHeader = header.replace(replaceHeader,'').replace(replaceSymbol,`${JSON.stringify(header[0])}:`).replaceAll('\'','\"').replaceAll('=>','\:').replace(replaceLastSymbol,'\"s\"\: {}');
-    // let slicedHeader = header.slice(header.indexOf("set-cookie")-1, header.length)
-    // let newHeader = slicedHeader.slice(0,slicedHeader.indexOf("]")+1)
-    // console.log("nh", newHeader.toString().slice(0,newHeader.toString().indexOf(':')))
-    // console.log('nh',{[newHeader.slice(0,newHeader.indexOf(':'))]: newHeader.slice(newHeader.indexOf(':')+1,newHeader.length)})
-    // return -1;
-    let objectFromHeaderArr = Object.entries(JSON.parse(newHeader))[0][1];
-    let sortCookies = objectFromHeaderArr['set-cookie'].split(';')
     
-    let finalCookies = Object.fromEntries(sortCookies.map((e,i)=>{
-      let idx = e.indexOf('=');
-      let key = e.slice((e[0] === ' ' ? 1:0), idx)
-      let tempkey  = key.split(',');
-      let finalkey = tempkey[1] ? tempkey[1].slice((tempkey[1][0] === ' ' ? 1:0), tempkey[1].length) : key;
-      let value =e.slice(idx+1, e.length);
-      return[finalkey,value ];
-    }))
-    // console.log("",finalCookies)
-   fs.writeFileSync(`${basepath}/cleanedResponseHeader.json`,JSON.stringify(finalCookies))
+    
   }
   var {email, username} = fetchCred();
   console.log(email, username);
@@ -175,21 +231,78 @@ async function login(counter = 0) {
             })
             .then(resp=> {return {res: resp.json(), resp:resp}}, rej => console.log('failed:', rej))
             .catch(err => {console.log(err)});
-  
-  if(lr.resp.status === '400'){signup() ;login(++counter)};
+  console.log("stat:", lr.resp.status)
+  if(lr.resp.status !== 200){console.log("bad request"); signup() ; setTimeout(() => {
+    login(++counter)
+  }, 2000); };
   let res = await lr.res;
 
   headerCapture.log(lr.resp.headers)//lr.resp.headers['set-cookie']);
   let cookie =  JSON.parse(JSON.stringify((fs.readFileSync("stringHeaderResponse.txt").toString())));
   // console.log("cookie", cookie)
   setCookies(cookie);
+  console.log("logged in:", lr.ok)
   return 1;
 }
 // login();
 // get user
-async function getUser(){
+// create app from cred
+async function createApp(app_count, id, csrftoken, sessionid){
+
+  let baseurl = `https://getstream.io/api/dashboard/organization/${id}/app/`
+  let cad = {
+              name: `${id}'s app${app_count}`, 
+              region: "tokyo", 
+              chat_region: "singapore", 
+              development_mode: true, 
+              template_app: ""
+  }
+  let jcad = JSON.stringify(cad);
+  let scad = `name=${cad.name}&region=${cad.region}&chat_region=${cad.chat_region}&development_mode=${cad.development_mode}&template_app=${cad.template_app}`
+  let cadh = {
+    "Referer": "https://dashboard.getstream.io/",
+    "path": `/api/dashboard/organization/${id}/app/`,
+    "User-Agent": "PostmanRuntime/7.29.0",
+    "Accept": "*/*",
+    "Host": "getstream.io",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Access-Control-Expose-Header": "set-cookie",
+    "Cookie":`csrftoken=${csrftoken}; sessionid=${sessionid}` ,
+    "x-csrftoken": `${csrftoken}`,
+    "Content-Type": "application/x-www-form-urlencoded",//"application/json",//"multipart/form-data; boundary=----WebKitFormBoundary40ftPa3qzAxhRqXt",//"application/x-www-form-urlencoded",
+    "Content-Length": scad.length
+    
+   //"Connection": "keep-alive",
+  }
+  let cadh_opts = { method: 'POST', // *GET, POST, PUT, DELETE, etc.
+              headers: cadh,
+              redirect: 'follow', // manual, *follow, error
+              referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+              body: scad // body data type must match "Content-Type" header
+            }
+  console.log({baseurl,cadh_opts})
+  // return -1;
+  //perform request
+  let car = await fetch(baseurl, cadh_opts)
+            .then(resp=> {return {res: resp.json(), resp:resp}}, rej => console.log('failed:', rej))
+            .catch(err => {console.log(err)});
+  let data = await car.res;
+  console.log("data", data)
+  return -1;
+}
+
+async function genApp(app_count){
   login();
-  let {csrftoken, sessionid} = JSON.parse(fs.readFileSync(`${process.cwd()}/cleanedResponseHeader.json`).toString())
+  let {csrftoken, sessionid} = await JSON.parse(fs.readFileSync(`${process.cwd()}/cleanedResponseHeader.json`).toString())
+  console.log("tok fail",csrftoken === undefined)
+  if(csrftoken === undefined) { 
+    console.log("retrying..")
+    setTimeout(() => {
+      return genApp(app_count)
+    }, 3000);
+    return 0;
+  };
+  // return -1;
   let baseurl = 'https://getstream.io/api/accounts/user/';
   let basepath = process.cwd();
   let guh = {
@@ -214,24 +327,30 @@ async function getUser(){
             .then(resp=> {return {res: resp.json(), resp:resp}}, rej => console.log('failed:', rej))
             .catch(err => {console.log(err)});
   
-  if(gr.resp.status === '400'){login()};
+  if(gr.resp.status !== 200){setTimeout(() => {
+      return login();
+    }, 3000); };
   let res = await gr.res
   console.log("final res:", res)
+  console.log("genned app in:",gr.ok)
+
   let [id, appkey, appsec] = [res.organizations[0].apps[0].id,res.organizations[0].apps[0].api_access[0].key, res.organizations[0].apps[0].api_access[0].secret];
   console.log(id, appkey, appsec)
-  // let cookieString = "id:"+id.toString()+"\n"
-  //                     +"appkey:"+appkey.toString()+"\n"
-  //                     +"appsec:"+appsec.toString()+"\n"
-  // // return fs.writeFileSync(`${basepath}/streamData.txt`,JSON.stringify({id,appkey,appsec}))
-  //   fs_do('write',basepath,'streaData.txt',cookieString)
-  //   fs_do('write',basepath,'streamData.json',JSON.stringify({id,appkey,appsec}))
+  let cookieString = "id:"+id.toString()+"\n"
+                      +"appkey:"+appkey.toString()+"\n"
+                      +"appsec:"+appsec.toString()+"\n"
+  // return fs.writeFileSync(`${basepath}/streamData.txt`,JSON.stringify({id,appkey,appsec}))
+  fs_do('write',basepath,'streaData.txt',cookieString)
+  fs_do('write',basepath,'streamData.json',JSON.stringify({id,appkey,appsec}))
+  console.log(id, csrftoken, sessionid)
+  createApp(app_count,id,csrftoken, sessionid)
+  return {id, csrftoken, sessionid};
 }
 
-getUser();
-// create app from cred
-function createApp(cred){
 
-}
+// login()
+genApp(4);
+// createApp(3)
 // regex
 // (\[*[a-zA-Z0-9]*\(*[a-zA-Z0-9]*\s*[a-zA-Z0-9]*\)\])
 // codnitional looknehind
